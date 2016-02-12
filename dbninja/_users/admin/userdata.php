2933882de3e44ea5f7e38d6e1ca7a3a536f70eb4
MehdiHenna <mehdihennaoui@gmail.com>
2016-02-12 11:56:27 +0100

    Migration faite et installation et mise en place de la base de données avec dbninja

---
+++ dbninja/_users/admin/userdata.php
@@ -0,0 +1,31 @@
+<?php
+return array (
+  'queryPageSize' => 100,
+  'queryMaxCellSize' => 1024,
+  'queryMaxDataSize' => 20,
+  'queryMaxRows' => 100000,
+  'reqTimeout' => 60,
+  'hideSysDBs' => false,
+  'showQueryRowNums' => true,
+  'binCellAction' => 0,
+  'tableAction' => 0,
+  'encrypt' => true,
+  'secureKey' => '',
+  'passwd' => 'd45336512578698404939dd35c2965e5baa88b3b',
+  'hosts' => 
+  array (
+    0 => 
+    array (
+      'label' => 'IntermediaireTaskList',
+      'host' => '127.0.0.1',
+      'port' => 3306,
+      'uname' => 'root',
+      'passwd' => 'root',
+      'queries' => 
+      array (
+      ),
+    ),
+  ),
+  'uid' => '86xbaderhcz96kbdcops',
+);
+?>
